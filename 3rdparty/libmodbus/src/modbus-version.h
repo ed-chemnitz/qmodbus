d34d8863437e8d3c9c26c89b4aaf40218add10cf
Tobias Doerffel <tobias.doerffel@gmail.com>
2013-12-11 21:22:18 +0100

    libmodbus: updated modbus-version.h with upstream version

--- 3rdparty/libmodbus/src/modbus-version.h
+++ 3rdparty/libmodbus/src/modbus-version.h
@@ -15,29 +15,29 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef _MODBUS_VERSION_H_
-#define _MODBUS_VERSION_H_
+#ifndef MODBUS_VERSION_H
+#define MODBUS_VERSION_H
 
 /* The major version, (1, if %LIBMODBUS_VERSION is 1.2.3) */
-#define LIBMODBUS_VERSION_MAJOR (2)
+#define LIBMODBUS_VERSION_MAJOR (3)
 
 /* The minor version (2, if %LIBMODBUS_VERSION is 1.2.3) */
-#define LIBMODBUS_VERSION_MINOR (9)
+#define LIBMODBUS_VERSION_MINOR (1)
 
 /* The micro version (3, if %LIBMODBUS_VERSION is 1.2.3) */
-#define LIBMODBUS_VERSION_MICRO (3)
+#define LIBMODBUS_VERSION_MICRO (1)
 
 /* The full version, like 1.2.3 */
-#define LIBMODBUS_VERSION        2.9.3
+#define LIBMODBUS_VERSION        3.1.1
 
 /* The full version, in string form (suited for string concatenation)
  */
-#define LIBMODBUS_VERSION_STRING "2.9.3"
+#define LIBMODBUS_VERSION_STRING "3.1.1"
 
 /* Numerically encoded version, like 0x010203 */
-#define LIBMODBUS_VERSION_HEX ((LIBMODBUS_MAJOR_VERSION << 24) |        \
-                               (LIBMODBUS_MINOR_VERSION << 16) |        \
-                               (LIBMODBUS_MICRO_VERSION << 8))
+#define LIBMODBUS_VERSION_HEX ((LIBMODBUS_VERSION_MAJOR << 24) |  \
+                               (LIBMODBUS_VERSION_MINOR << 16) |  \
+                               (LIBMODBUS_VERSION_MICRO << 8))
 
 /* Evaluates to True if the version is greater than @major, @minor and @micro
  */
@@ -49,4 +49,4 @@
       LIBMODBUS_VERSION_MINOR == (minor) &&             \
       LIBMODBUS_VERSION_MICRO >= (micro)))
 
-#endif /* _MODBUS_VERSION_H_ */
+#endif /* MODBUS_VERSION_H */
